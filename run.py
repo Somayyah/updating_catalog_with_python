30e77a9d4a556d552068b0a89f4eb91756465ae9
Somayyah <55834191+Somayyah@users.noreply.github.com>
2020-09-14 21:04:30 +0300

    Update run.py

--- run.py
+++ run.py
@@ -4,22 +4,26 @@ import os
 import requests
 
 entry = {
-	"name": "",
-	"weight": 0,
-	"description": "",
-	"image_name": ""
-	}
+    'name': '',
+    'weight': 0,
+    'description': '',
+    'image_name': '',
+    }
 
 dist_dir = './supplier-data/images/'
-url = 'http://localhost/upload/'
+url = 'http://localhost/fruits/'
 
 for image in os.listdir(dist_dir):
     if 'jpeg' in image:
         image_name = image.split('.')[0]
-	desc_path = './supplier-data/descriptions/' + image_name + ".txt"
-	with open(desc_path, 'r') as file:
-		for i in ["name", "weight", "description"]:
-			entry[i] = file.readline()
-	entry["image_description"] = dist_dir + image
-	r = requests.post(url, data=entry, files={'file': image})
+        desc_path = './supplier-data/descriptions/' + image_name \
+            + '.txt'
+        with open(desc_path, 'r') as file:
+            for i in ['name', 'weight', 'description']:
+                entry[i] = file.readline()
+        entry['weight'] = int(entry['weight'].split()[0])
+        entry['image_name'] = image
+
+        r = requests.post(url, data=entry)
+
 
